e142167993dcf5659ac0851b78a678f80be084a1
Indrani Bandyopadhyay <IN40068837@wipro.com>
2024-02-07 14:47:48 +0530

    vulnerabilities fixed

--- src/main/java/com/iemr/common/controller/nhmdashboard/NHMDetailCallReportScheduler.java
+++ src/main/java/com/iemr/common/controller/nhmdashboard/NHMDetailCallReportScheduler.java
@@ -6,7 +6,6 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.apache.xml.security.stax.config.ConfigurationProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
