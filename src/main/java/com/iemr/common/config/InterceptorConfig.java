a76c4df1031f683b9d18df3d62cc95a13e1db8d8
devikasuresh20 <57424483+devikasuresh20@users.noreply.github.com>
2024-02-17 20:52:45 +0530

    Removed myapp properties (#89)
    * Added limit condition while fetching Item stock entry details in eaushadha API
    
    * Minor modifications in 1097 referral sms template
    
    * Added institute name along with referral SMS
    
    * Removed myapp propeties and minor change in interceptor.
    
    ---------
    
    Co-authored-by: Devika <de40034072@wipro.com>

--- src/main/java/com/iemr/common/config/InterceptorConfig.java
+++ src/main/java/com/iemr/common/config/InterceptorConfig.java
@@ -29,21 +29,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import com.iemr.common.utils.http.HTTPRequestInterceptor;
 
 @Configuration
-public class InterceptorConfig extends WebMvcConfigurerAdapter
-{
+public class InterceptorConfig extends WebMvcConfigurerAdapter {
 
 	@Autowired
 	HTTPRequestInterceptor requestInterceptor;
 
-//	@Override
-//	public void addInterceptors(InterceptorRegistry registry)
-//	{
-//		registry.addInterceptor(requestInterceptor);
-//	}
-	
 	@Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new BlockingHttpMethodInterceptor())
-                .addPathPatterns("/**"); 
-    }
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(requestInterceptor);
+	}
+
 }
