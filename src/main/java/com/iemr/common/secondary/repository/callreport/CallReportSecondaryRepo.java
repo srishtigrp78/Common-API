a432073991d49b2f869e1e9c79141180b88133ac
appuser <appuser@wipro-test-serv>
2024-03-04 20:17:46 +0530

    issue fixed

--- src/main/java/com/iemr/common/secondary/repository/callreport/CallReportSecondaryRepo.java
+++ src/main/java/com/iemr/common/secondary/repository/callreport/CallReportSecondaryRepo.java
@@ -22,16 +22,12 @@
 package com.iemr.common.secondary.repository.callreport;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import com.iemr.common.data.report.CallReport;
 
 import common.iemr.common.secondary.data.report.SecondaryCallReport;
 
