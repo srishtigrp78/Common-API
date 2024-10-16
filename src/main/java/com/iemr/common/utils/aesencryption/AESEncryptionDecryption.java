a7a9518b7a432a1e31270df03bc49239befc73d6
SR20290919 <SR20290919@APL-PG02PW8B>
2024-10-15 23:34:46 -0700

    adding changes related to encryption and decryption

--- src/main/java/com/iemr/common/utils/aesencryption/AESEncryptionDecryption.java
+++ src/main/java/com/iemr/common/utils/aesencryption/AESEncryptionDecryption.java
@@ -1,73 +1,114 @@
 package com.iemr.common.utils.aesencryption;
 
 import java.io.UnsupportedEncodingException;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Base64;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.GCMParameterSpec;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import com.iemr.common.encryption.exception.DecryptionException;
-import com.iemr.common.encryption.exception.EncryptionException;
 import com.iemr.common.utils.aesencryption.AESEncryptionDecryption;
 
 
 @Component
 public class AESEncryptionDecryption {
 
-	private static  Logger logger = LoggerFactory.getLogger(AESEncryptionDecryption.class);
+	
+	private Logger logger = LoggerFactory.getLogger(AESEncryptionDecryption.class);
+
 	private static SecretKeySpec secretKey;
-	private static  byte[] key;
-	static final String SECRET = "amrith$%2022@&*piramal@@swasthya!#";
 
-	public static void setKey(String myKey) {
-		MessageDigest sha = null;
+	private byte[] key;
+
+	private final String secret = "amrith$%2022@&*piramal@@swasthya!#";
+
+	private static final int IV_SIZE = 12;
+
+	private static final int TAG_SIZE = 128;
+
+	private final String UTF_8 = "UTF-8";
+ 
+	public void setKey(String myKey) {
+
 		try {
-			key = myKey.getBytes("UTF-8");
-			sha = MessageDigest.getInstance("SHA-512");
+
+			key = myKey.getBytes(UTF_8);
+
+			MessageDigest sha = MessageDigest.getInstance("SHA-512");
+
 			key = sha.digest(key);
+
 			key = Arrays.copyOf(key, 16);
+
 			secretKey = new SecretKeySpec(key, "AES");
+
 		} catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
-			 logger.error("context", e);
+
+			logger.error("context", e);
+
 		}
+
 	}
+ 
+	public String encrypt(String strToEncrypt) throws Exception {
 
+		if (secretKey == null)
 
-	public  String encrypt(String strToEncrypt) throws EncryptionException {
-		 String encryptedString=null;
-		try {
-			if (secretKey == null)
-			     setKey(SECRET);
-			Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-			encryptedString= Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
-		} catch (Exception e) {
-			logger.error("Error while encrypting: {}", e.toString());
-			throw new EncryptionException("Error while encrypting: "+e.toString(), e);
-		}
-		return encryptedString;
+			setKey(secret);
+
+		Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+ 
+		// Generate IV
+
+		byte[] iv = new byte[IV_SIZE];
+
+		SecureRandom random = new SecureRandom();
+
+		random.nextBytes(iv);
+
+		cipher.init(Cipher.ENCRYPT_MODE, secretKey, new GCMParameterSpec(TAG_SIZE, iv));
+ 
+		byte[] encryptedBytes = cipher.doFinal(strToEncrypt.getBytes(UTF_8));
+
+		byte[] encryptedIvAndText = new byte[IV_SIZE + encryptedBytes.length];
+
+		System.arraycopy(iv, 0, encryptedIvAndText, 0, IV_SIZE);
+
+		System.arraycopy(encryptedBytes, 0, encryptedIvAndText, IV_SIZE, encryptedBytes.length);
+ 
+		return Base64.getEncoder().encodeToString(encryptedIvAndText);
+
 	}
+ 
+	public String decrypt(String strToDecrypt) throws Exception {
 
+		if (secretKey == null)
 
-	public  String decrypt(String strToDecrypt) throws DecryptionException {
-		 String decryptedString=null;
-		try {
-			if (secretKey == null)
-				setKey(SECRET);
-			Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-			cipher.init(Cipher.DECRYPT_MODE, secretKey);
-			decryptedString= new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
-		} catch (Exception e) {
-			logger.error("Error while decrypting: {}",e.toString());
-			throw new DecryptionException("Error while decrypting: "+e.toString(), e);
-		}
-		return decryptedString;
+			setKey(secret);
+ 
+		byte[] encryptedIvAndText = Base64.getDecoder().decode(strToDecrypt);
+
+		byte[] iv = Arrays.copyOfRange(encryptedIvAndText, 0, IV_SIZE);
+
+		byte[] encryptedBytes = Arrays.copyOfRange(encryptedIvAndText, IV_SIZE, encryptedIvAndText.length);
+ 
+		Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+
+		cipher.init(Cipher.DECRYPT_MODE, secretKey, new GCMParameterSpec(TAG_SIZE, iv));
+
+		byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+ 
+		return new String(decryptedBytes, UTF_8);
+
 	}
+ 
 }
